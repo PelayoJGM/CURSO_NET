74d756fe6c39279a9fdaa07997b0e3f1027cfd77
PelayoFloxer <75483664+PelayoFloxer@users.noreply.github.com>
2021-02-23 16:54:41 +0100

    cambios tarde

---
+++ Madison/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madison
+{
+    public class Book : Product
+    {
+        private string Author;
+        private string Isbn;
+
+        public Book(int code, double price, string description, string author, string isbn) : base(code, price, description)
+
+        {
+            Author = author;
+            Isbn = isbn;
+
+        }
+    }
+}
