600d2f1999d1c5975d9b05c8b781bf96f59a9476
Pelayo <pelayo.contact@aol.com>
2021-02-23 13:28:36 +0100

    Subida Madison

---
+++ Madison/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madison
+{
+    class Product
+    {
+        protected double salesPrice;
+        protected double factoryPrice;
+        protected string productName;
+
+        public Product(string name, double price, double cost)
+
+        {
+
+            productName = name;
+            factoryPrice = price;
+            salesPrice = price;
+
+           
+    }
+
+    }
+   
+}
